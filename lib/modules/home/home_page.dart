ecebe955d43a25c98ec14d99bd40f647b2428494
xDust <leonardoctk3@gmail.com>
2021-07-02 18:40:31 -0300

    melhorada a apresentação do logo do app na homepage

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -11,37 +11,42 @@ class HomePage extends ConsumerWidget {
   const HomePage({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context, ScopedReader watch) {
-    final size = MediaQuery.of(context).size;
-    final logoSizeWidth = size.width * 0.12;
-    final logoSizeHeight = size.width * 0.11;
+    final size = MediaQuery.of(context).size.width;
+    final sizeHeight = MediaQuery.of(context).size.height;
+    final logoSizeWidth = size * 0.12;
+    final logoSizeHeight = sizeHeight * 0.11;
+    double topPadding = sizeHeight * 0.014;
     return Scaffold(
       body: Stack(
         children: [
           PopularMovies(),
-          Padding(
-            padding: const EdgeInsets.only(top: 34, left: 6),
+          Positioned(
+            top: topPadding,
             child: Container(
               alignment: Alignment.topLeft,
               height: logoSizeHeight,
-              constraints: BoxConstraints(maxHeight: 60),
-              child: Stack(children: <Widget>[
-                Opacity(
-                    child: Image.asset(AppImages.logoHome,
+              constraints: BoxConstraints(maxHeight: logoSizeHeight),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Stack(children: <Widget>[
+                  Opacity(
+                      child: Image.asset(AppImages.logoHome,
+                          width: logoSizeWidth,
+                          height: logoSizeHeight,
+                          color: Colors.black),
+                      opacity: 0.2),
+                  ClipRect(
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
+                      child: Image.asset(
+                        AppImages.logoHome,
                         width: logoSizeWidth,
                         height: logoSizeHeight,
-                        color: Colors.black),
-                    opacity: 0.2),
-                ClipRect(
-                  child: BackdropFilter(
-                    filter: ImageFilter.blur(sigmaX: 5.0, sigmaY: 5.0),
-                    child: Image.asset(
-                      AppImages.logoHome,
-                      width: logoSizeWidth,
-                      height: logoSizeHeight,
+                      ),
                     ),
                   ),
-                ),
-              ]),
+                ]),
+              ),
             ),
           ),
           searchBarUI(context),
